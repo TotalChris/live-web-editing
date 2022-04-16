d739d65730df304246a2561fff3815838e5fa277
Chris Yates <chris.m.yates@outlook.com>
2022-04-16 17:23:38 -0400

    Input box now operates instantly and independently
    - Move progress bar into LiveBox element
    - Perform instant submission on any input change
    - Progress bar remains disabled until input occurs
    - Clean unused code from index.html and LiveBox.js

--- livebox.js
+++ livebox.js
@@ -16,6 +16,13 @@ export default class LiveBox extends LitElement {
     firstUpdated() {
         this.t = {type: null, p1: null, p2: null, content: null};
         this.hist = new LinkedList();
+        this.wheel = this.renderRoot.querySelector("#steeringwheel");
+        this.wheel.min = -1;
+        this.wheel.max = -1;
+        this.wheel.value = -1;
+        this.wheel.disabled = true;
+        this.histArray;
+        this.lastIndex;
         this.renderRoot.querySelector('#mainbox').addEventListener('beforeinput', (e) => {
             this.x = e.target.selectionStart;// x is cursor position before op
             if(e.inputType == "insertLineBreak"){
@@ -23,6 +30,7 @@ export default class LiveBox extends LitElement {
             }
         })
         this.renderRoot.querySelector('#mainbox').addEventListener('input', (e) => {
+            this.wheel.disabled = false;       
             this.y = e.target.selectionStart;// y is cursor position after op
             this.o = this.n;
             this.n = e.target.value; //TODO: Chunk of string
@@ -37,11 +45,24 @@ export default class LiveBox extends LitElement {
                     this.t = {content: this.o.substring(this.y, this.x), type: "sub", p1: this.y, p2: this.x} 
                     break;
             }
-            //input proof separate here
             this.hist.append(this.t);
+            this.histArray = this.hist.traverse();
+            this.wheel.max = this.histArray.length - 1;
+            this.wheel.value = this.histArray.length - 1;
+            this.lastIndex = this.histArray.length - 1;
+            //input proof separate here
             //document.querySelector('#copy').innerHTML = this.place(document.querySelector('#copy').innerHTML, (this.t.type == "add" ? this.t.content : ""), this.t.p1, (this.t.type == "sub" ? this.t.p2 : null));
-            
         });
+        this.renderRoot.querySelector("#steeringwheel").addEventListener("input", (v) => {
+            let i = parseFloat(v.target.value)
+            let dir = (this.lastIndex < i? "fwd" : "bck" )
+            for(let p = this.lastIndex; p != i;p = p + (dir == "bck" ? -1 : 1)){
+                let entry = this.histArray[p + (dir == "bck" ? 0 : 1)]
+                let ttype = (((entry.type == "add" && dir == "fwd")||(entry.type == "sub" && dir == "bck")) ? "add" : "") + (((entry.type == "sub" && dir == "fwd")||(entry.type == "add" && dir == "bck")) ? "sub" : "") 
+                this.value = this.value.slice(0, entry.p1) + (ttype == "add" ? entry.content : "") + this.value.slice((ttype == "sub" ? entry.p2 : null) || entry.p1);
+            }
+            this.lastIndex = i;
+        })
     }
     static get styles(){
         return css`
@@ -49,7 +70,7 @@ export default class LiveBox extends LitElement {
             font-family: "Product-Sans";
             src: url("ProductSans.ttf");
         }
-        input{
+        textarea{
             font-family: "Product-Sans";
             font-size: 25px;
             outline: none;
@@ -58,6 +79,10 @@ export default class LiveBox extends LitElement {
             border-radius: 15px;
             height: 50px;
             width: 450px;
+            padding: 10px;
+        }
+        #steeringwheel{
+            width: 450px;
             padding-left: 10px;
             padding-right: 10px;
         }
@@ -65,7 +90,9 @@ export default class LiveBox extends LitElement {
     }
     render() {
         return html`
-            <input id="mainbox"></input>
+            <textarea id="mainbox"></textarea><br>
+            <input type="range" id="steeringwheel">
+
         `
     }
 }
